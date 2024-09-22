20caea4382c9e9405418d9d259633c4ef18db6ce
Carlos Martinez <2730538920101@ingenieria.usac.edu.gt>
2024-09-22 08:55:12 -0600

    [ADD] Authorization & Autentication

--- backend/app_service/app/models/factory_method/cliente.py
+++ backend/app_service/app/models/factory_method/cliente.py
@@ -1,7 +1,7 @@
 from .user import User
 
 class Cliente(User):
-    def __init__(self, nombre, fecha_nacimiento, genero, correo, foto_dpi, telefono, contrasenia, pregunta, respuesta):
+    def __init__(self, nombre, fecha_nacimiento, genero, correo, foto_dpi, telefono, contrasenia):
         self.nombre = nombre
         self.fecha_nacimiento = fecha_nacimiento
         self.genero = genero
@@ -9,8 +9,7 @@ class Cliente(User):
         self.foto_dpi = foto_dpi
         self.telefono = telefono
         self.contrasenia = contrasenia
-        self.pregunta = pregunta
-        self.respuesta = respuesta
+
     
 
     def get_role(self):
--- backend/app_service/app/routes/administrador.py
+++ backend/app_service/app/routes/administrador.py
@@ -5,6 +5,7 @@ from ..controllers.administrador_controller import AdministradorController
 from ..controllers.encryption_controller import EncryptionController
 from ..services.s3_service import S3Service
 from ..services.cognito_service import CognitoService
+from ..services.auth_service import AuthService  # Importar el AuthService
 from ..models.factory_method.factory import UserFactory
 from config import Config
 
@@ -17,4 +18,40 @@ administrador_controller = AdministradorController()
 encryption_controller = EncryptionController() 
 s3_service = S3Service()
 cognito_service = CognitoService()
+auth_service = AuthService()
 
+# Ruta para bloquear una cuenta
+@bp.route('/block-account', methods=['POST'])
+@auth_service.is_authorized(['AdminGroup'])
+def block_account():
+    data = request.get_json()
+    email = data.get('email')
+
+    if not email:
+        return jsonify({"error": "Email is required"}), 400
+
+    # Bloquear cuenta
+    response = cognito_service.block_account(email)
+    
+    if response:
+        return jsonify({"message": "Account blocked successfully"}), 200
+    else:
+        return jsonify({"error": "Failed to block account"}), 400
+
+# Ruta para desbloquear una cuenta
+@bp.route('/unblock-account', methods=['POST'])
+@auth_service.is_authorized(['AdminGroup'])
+def unblock_account():
+    data = request.get_json()
+    email = data.get('email')
+
+    if not email:
+        return jsonify({"error": "Email is required"}), 400
+
+    # Desbloquear cuenta
+    response = cognito_service.unblock_account(email)
+    
+    if response:
+        return jsonify({"message": "Account unblocked successfully"}), 200
+    else:
+        return jsonify({"error": "Failed to unblock account"}), 400
